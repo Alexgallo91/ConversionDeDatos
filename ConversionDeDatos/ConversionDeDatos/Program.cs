0b992ccd9885e97afd5688e37e6fabff097f479d
Abraham Araujo Moreno <alex_91y@hotmail.com>
2016-12-01 00:22:16 -0600

    Add files via upload

---
+++ ConversionDeDatos/ConversionDeDatos/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversionDeDatos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //conversion implicita
+            int i = 100;
+            float f = i;
+            Console.WriteLine(f);
+
+            //conversion explicita
+            float f2 = 123.43f;
+            int i2 = (int)f2;
+            Console.WriteLine(i2);
+
+            Console.ReadKey();
+        }
+    }
+}
