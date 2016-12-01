1a81d1de5738fad8b2d65e4471db4351d8565443
Abraham Araujo Moreno <alex_91y@hotmail.com>
2016-12-01 00:22:52 -0600

    Add files via upload

--- ConversionDeDatos/ConversionDeDatos/Program.cs
+++ ConversionDeDatos/ConversionDeDatos/Program.cs
@@ -4,22 +4,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConversionDeDatos
+namespace ConversionDeTipos
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //conversion implicita
             int i = 100;
             float f = i;
-            Console.WriteLine(f);
-
-            //conversion explicita
-            float f2 = 123.43f;
+            float f2 = 123.34f;
+            float f3 = 122.1212f;
+            int i3 = Convert.ToInt16(f3);
             int i2 = (int)f2;
-            Console.WriteLine(i2);
+            String number = "100";
+            int numero = int.Parse(number);
+            int result;
 
+            String number2;
+            Console.WriteLine("Ingresa un numero:");
+            number2 = Console.ReadLine();
+
+            bool conversion = int.TryParse(number2, out result);
+
+            if (conversion)
+            {
+                Console.WriteLine("La conversion fue exitosa");
+                Console.WriteLine("result = {0}", result);
+            }
+            else
+            {
+                Console.WriteLine("No se pudo llevar la consersion!!!");
+            }
+
+            Console.WriteLine(f);
+            Console.WriteLine(f2);
+            Console.WriteLine(numero);
             Console.ReadKey();
         }
     }
